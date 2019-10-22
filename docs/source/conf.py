8f359f884bae8632423dadebbe3c3d8a28827ee1
Davide <davidonzo@gmail.com>
2019-10-22 17:09:07 +0200

    Fixed doc version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Davide Baglieri'
 author = 'Davide Baglieri'
 
 # The full version, including alpha/beta/rc tags
-release = '1.7'
+release = '1.8'
 
 
 # -- General configuration ---------------------------------------------------
